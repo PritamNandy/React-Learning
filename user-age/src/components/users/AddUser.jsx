e8179db778b1b6813a68fbdb7a2beb4e7e758ce7
Prasun Nandy Pritam <pritamnandy84@yahoo.com>
2023-01-23 00:50:53 +0600

    23th Jan

--- user-age/src/components/users/AddUser.jsx
+++ user-age/src/components/users/AddUser.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../ui/Button';
 import Card from '../ui/Card';
 import classes from './AddUser.module.css'
 
 export default function AddUser() {
     const addUserHandler = (event) => {
         event.preventDefault();
+        if(username.trim().length === 0 || age.trim().length === 0) {
+          return;
+        }
+        if(+age < 0) {
+          return;
+        }
+        setUsername('')
+        setAge('')
+    }
+    const [username, setUsername] = useState('')
+    const [age, setAge] = useState('')
+
+    const onUserInputChange = (event) => {
+      if(event.target.name === 'username') {
+        setUsername(event.target.value)
+      }
+
+      if(event.target.name === 'age') {
+        setAge(event.target.value)
+      }
     }
 
   return (
     <Card classes={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id='username' type="text" /> 
+            <input id='username' type="text" value={username} name="username" onChange={onUserInputChange} /> 
 
             <label htmlFor="age">Age (years)</label>
-            <input id='age' type="number" /> 
+            <input id='age' type="number" value={age} name="age" onChange={onUserInputChange} /> 
 
-            <button type='submit'>Add User</button>
+            <Button type='submit'>Add User</Button>
         </form>
     </Card>
     
